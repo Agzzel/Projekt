4d1942a866058b8bec931b51a3e32fe0d46586cd
Ackinator <sterneraxel@protonmail.com>
2024-10-16 10:24:01 +0200

    Lagt in felhantering i sniffer

--- sniffer.py
+++ sniffer.py
@@ -1,5 +1,5 @@
 #   Spara packets som flödar i nätverket i en .pcap-fil för analys i t.ex. WireShark.
-#   Användaren ska kunna specificera hur många packets som ska avlyssnas (t.ex. 25 packets)
+#   Användaren ska kunna specificera hur många packets som ska sparas (t.ex. 25 packets)
 #   Anv. ska välja vilket protokoll som ska avlyssnas, standard är TCP.
 
 # Lägg in felhantering
@@ -9,12 +9,17 @@ import argparse
 from scapy.all import *
 
 parser = argparse.ArgumentParser(description="Simple Packet Sniffer")
-parser.add_argument("count", help="Specify how many packets should be sniffed")
-parser.add_argument("protocol", help="Specify protocol to sniff")
-parser.add_argument("filename", help="Name of the .pcap file that stores all the packets")
-parser.add_argument("interface", help="Specify which network interface to sniff")
+parser.add_argument("count", type=int, help="Specify how many packets should be sniffed")
+parser.add_argument("protocol", type=str, help="Specify protocol to sniff")
+parser.add_argument("filename", type=str, help="Name of the .pcap file that stores all the packets")
 
-args = parser.parse_args()
-capture = sniff(count = int(args.count), filter=args.protocol, iface=args.interface)
-capture.summary()
-wrpcap(args.filename, capture) 
+
+def performSniff(packetNum, protocol, filename):
+    capture = sniff(count=packetNum, filter=protocol)
+    capture.summary()
+    wrpcap(filename, capture)
+try:
+    args = parser.parse_args()
+    performSniff(args.count, args.protocol, args.filename) 
+except PermissionError:
+    print("Error: this program can only be executed by root")
